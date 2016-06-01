c32b01715b8181bd71cd3c7982d43cae4eed9e2a
Janos, Benjamin <bjanos0000@hqsml-143813.local>
2016-06-01 11:59:07 -0600

    Initial Commit

---
+++ SuperCool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperCool
+//
+//  Created by Janos, Benjamin on 6/1/16.
+//  Copyright © 2016 Janos, Benjamin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
